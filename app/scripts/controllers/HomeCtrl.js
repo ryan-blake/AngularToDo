4612a4d457c4f455073803e2a0e296129b540375
Ryan Kliemann <Ryan_blake@icloud.com>
2016-05-31 11:51:39 -0500

    check t/f createdAt ng-show. processing

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,60 +1,66 @@
-var app = angular.module("sampleApp", ["firebase"]);
-// var app = angular.module("todoApp", ["firebase"]);
+var app = angular.module("2DoApp", ["firebase"]);
 
-app.factory("chatMessages", ["$firebaseArray",
-// app.factory("todoLists", ["$firebaseArray",
+app.factory("todoLists", ["$firebaseArray",
 
   function($firebaseArray) {
     // create a reference to the database where we will store our data
-    var randomRoomId = Math.round(Math.random() * 100000000);
-    // var randomRoomID = 1 ---room id irrelevant search by user/complete/text/timestamp
+    // var randomRoomId = Math.round(Math.random() * 100000000);
+    var randomRoomID = 1
+    //  ---room id irrelevant search by user/complete/text/timestamp
     var ref = new Firebase("https://what2do-4dd97.firebaseio.com/" );
 
     return $firebaseArray(ref);
   }
 ]);
 
-app.controller("ChatCtrl", ["$scope", "chatMessages",
-// app.controller("todoCtrl", ["$scope", "todoList",
-
-  function($scope, chatMessages) {
-    // function($scope, todoLists) {
-
-    $scope.user = "Guest " + Math.round(Math.random() * 100);
-    // $scope.authUser= message.user ------unsure for now
-
-    $scope.messages = chatMessages;
-    // $scope.todos = todoLists;
 
 
-    $scope.addMessage = function() {
+app.controller("todoCtrl", ["$scope", "todoLists",
+
+    function($scope, todoLists) {
+
+    $scope.user = "";
+
+    $scope.todos = todoLists;
+
+
+    $scope.addTodo= function() {
       // addTodo
       // $add on a synchronized array is like Array.push() except it saves to the database!
-      $scope.messages.$add({
-        // $scope.todos.$add({
-        from: $scope.user,
-        content: $scope.message,
-        // content: $scope.todo,
-        timestamp: Firebase.ServerValue.TIMESTAMP
+        $scope.todos.$add({
 
-      });
+          from: $scope.user,
+          content: $scope.todo,
+          createdAt: Firebase.ServerValue.TIMESTAMP,
+          completed : false 
+          // completed: $scope.checked
 
-      $scope.message = "";
-      // $scope.todo = "";
+
+        });
+      $scope.todo = "";
     };
 
     // if the messages are empty, add something for fun!
     // disect addition of todo format
-    $scope.messages.$loaded(function() {
-      if ($scope.messages.length === 0) {
-        $scope.messages.$add({
+    $scope.todos.$loaded(function() {
+      if ($scope.todos.length === 0) {
+        $scope.todos.$add({
           from: "UserSays",
           content: "Enter what 2 do!",
           timestamp: Firebase.ServerValue.TIMESTAMP
         });
-     }
+       }
     });
+
+    // $scope.lessThanWeekOld = function() {
+    //   timeElapsed = Date.now - createdAt;
+    //   if (timeElapsed >=  6048000000)
+    //    return true;
+    //    else
+    //    return false;
+    // };
   }
+
 
 
 ]);
---
+++ app/scripts/controllers/fixtures/time.js
@@ -0,0 +1,7 @@
+// only show todo's are less than 7 days old
+// function lessThanWeekOld takes in createdAt (ms) todo.createdAt
+//   timeElapsed = current time - createdAt
+//   if timeElapsed <= 7 days in ms
+//     return true
+//   else
+//     return false
